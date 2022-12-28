a859348811962beb93cf4a55cb3d1c49f6f4fac5
Ali Moradzade <alimorizz1379@gmail.com>
2022-12-28 09:31:50 +0330

    adding Service interface, adding validate, handle get&post to our sdk call method

--- ClientSDK.ts
+++ ClientSDK.ts
@@ -1,27 +1,32 @@
 import * as fs from 'fs';
 import * as yaml from 'js-yaml';
+import axios from "axios";
+import {Env} from "./Env";
 
 // Based on config.yaml file
+interface Service {
+    name: string,
+    url: string,
+    method: string,
+    payload: {
+        [p: string]: any
+    }
+}
+
 interface Config {
     main: {
         address: string;
         sandboxAddress: string;
     },
-    services: {
-        name: string;
-        url: string;
-        method: string;
-        payload: {
-            [key: string]: any;
-        }
-    }[]
+    services: Service[]
 }
 
 export class ClientSDK {
     private readonly yamlConfigFilePath = 'config.yaml';
     private readonly config: Config;
+    private readonly bearerToken = Env.bearerToken;
 
-    constructor(private readonly clientId: string, private readonly clientPassword: string) {
+    constructor(private readonly clientId: string, private readonly clientPassword: string,) {
         // Read config.yaml file
         try {
             let fileContents = fs.readFileSync('./config.yaml', 'utf8');
@@ -35,17 +40,59 @@ export class ClientSDK {
 
             console.log(`Loaded config from ${this.yamlConfigFilePath} file successfully ..`);
         } catch (e) {
-            console.log(e);
+            throw new Error(`Failed to load config from ${this.yamlConfigFilePath} file`);
         }
     }
 
-    callService(serviceName: string, payload: any) {
-        // Call service
-        const ourService = this.config.services.find(s => s.name === serviceName);
-        if (ourService) {
-            console.log(ourService)
-        } else {
-            console.log(`Service ${serviceName} not found in config file`);
+
+    async callService(serviceName: string, payload: any) {
+        const service = this.validate(serviceName, payload);
+
+        // Call service, with axios
+        if (service.method === 'get') {
+            return await this.handleGetRequest(service, payload);
+        } else if (service.method === 'post') {
+            return await this.handlePostRequest(service, payload);
         }
+    }
+
+    validate(serviceName: string, payload: any): Service {
+        const ourService = this.config.services.find(s => s.name === serviceName);
+        if (!ourService) {
+            throw new Error(`Service ${serviceName} not found in config file`);
+        }
+        console.log(ourService)
+
+        // Check if payload is valid
+        if (Object.keys(payload) !== Object.keys(ourService.payload)) {
+            throw new Error(`Invalid payload for service ${serviceName}`);
+        }
+
+        return ourService;
+    }
+
+    async handleGetRequest(service: Service, uriParameters: any) {
+        const {data, status} = await axios.get(service.url, {
+            headers: {Authorization: `Bearer ${this.bearerToken}`},
+            params: uriParameters
+        });
+
+        if (status !== 200) {
+            throw new Error(`Failed to call service ${service.name}`);
+        }
+
+        return data;
+    }
+
+    async handlePostRequest(service: Service, body: any)  {
+        const {data, status} = await axios.post(service.url, body, {
+            headers: {Authorization: `Bearer ${this.bearerToken}`}
+        });
+
+        if (status !== 200) {
+            throw new Error(`Failed to call service ${service.name}`);
+        }
+
+        return data;
     }
 }
