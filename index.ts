d08f1f829a86f65fd90563522018ee48f00a7ef8
Ali Moradzade <alimorizz1379@gmail.com>
2022-12-28 11:32:02 +0330

    changing our code to use try/catch

--- index.ts
+++ index.ts
@@ -1,15 +1,36 @@
 import {ClientSDK} from "./ClientSDK";
 import {Env} from "./Env";
+import { v4 as uuid } from 'uuid';
 
-const clientSDK = new ClientSDK(Env.clientId);
-clientSDK.callService('cardToIban', {
-    trackId: "cardToIban-027",
-    card: "6280231304985178",
-    version: "2"
-})
-    .then(data => {
-        console.log(data);
-    })
-    .catch(err => {
-        console.log(err.message)
-    })
+async function main() {
+    const clientSDK = new ClientSDK(Env.clientId);
+
+    try {
+        const result = await clientSDK.callService('cardToIban', {
+            trackId: uuid(),
+            card: "6280231304985178",
+            version: "2"
+        })
+
+        console.log(result);
+    } catch (e) {
+        console.error((e as Error).message);
+    }
+    try {
+
+        const result = await clientSDK.callService('billingInquiry', {
+            trackId: uuid(),
+            type: "Tel",
+            parameter: "02177689361",
+            secondParameter: "MCI"
+        })
+
+        console.log(result)
+    } catch (e) {
+        console.error((e as Error).message);
+    }
+
+}
+
+main();
+
