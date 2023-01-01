9b67e4a8c1d18bb19001f42efae6d8d41a240b26
Ali Moradzade <alimorizz1379@gmail.com>
2023-01-01 18:35:58 +0330

    adding get bearer token function, adding redis url to our constructor

--- src/ClientSDK.ts
+++ src/ClientSDK.ts
@@ -1,6 +1,7 @@
 import * as fs from 'fs';
 import * as yaml from 'js-yaml';
 import axios, {AxiosError} from "axios";
+import {createClient, RedisClientType} from 'redis';
 
 // Based on config.yaml file
 interface Service {
@@ -23,9 +24,11 @@ interface Config {
 export class ClientSDK {
     private readonly yamlConfigFilePath = 'config.yaml';
     private readonly config: Config;
-    private readonly bearerToken = "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbklkIjoiZThmMjBhMDYtNjEzZi00ODBmLTkyODEtYTA2Yzk5N2Q3MDIzIiwicmVmcmVzaFRva2VuIjoianB4bjlmYjFyeUlXa1lEemprR1FzTlRtY1plUzluQXhjNXg5UmZZRlNhaUJPM0psWnFNWUJzbWpZOVZsZVZ1cTBCRTdTc1psMEs4QVlwR2ZhV3BBWjBJREhPbXk0Z0wxZFpSd1c3NlR4TEw5WVpsVHJ0VTJZRFRaR093b3NRcXNoWlRFclFrN1p2UlQxaG9yU0IyTm5LQUF2WElsTzBjMDI5alo5cWs2eU9jNDVnNWNTR1RjbWVKcGFSRUluclJ4N3NUNmxTTXlTMk5QM254ang0UmVKVElyRnNlU3FTemFNaFh4bURLb0E3Q0xyMjdzbGdxMkUycUg1MVQ1cTlrQSIsImNyZWF0aW9uRGF0ZSI6IjE0MDExMDExMTIzODE1IiwibGlmZVRpbWUiOjg2NDAwMDAwMCwiY2xpZW50SWQiOiJhbGltb3JhZHphZGUiLCJ1c2VySWQiOiI0MDAwMzI5NzY2IiwiYWN0aXZlIjp0cnVlLCJzY29wZXMiOlsib2FrOmliYW4taW5xdWlyeTpnZXQiLCJvYWs6Z3JvdXAtaWJhbi1pbnF1aXJ5OnBvc3QiLCJvYWs6Z3JvdXAtaWJhbi1pbnF1aXJ5OmdldCIsIm9hazpjYXJkLXRvLWRlcG9zaXQ6Z2V0Iiwib2FrOmRlcG9zaXQtdG8taWJhbjpnZXQiLCJvYWs6Y2FyZC1zdGF0ZW1lbnQ6Z2V0Iiwib2FrOmNhcmQtYmFsYW5jZTpnZXQiLCJvYWs6Y2hlY2stdXNlcjpnZXQiLCJvYWs6Y2lmLWlucXVpcnk6Z2V0Iiwib2FrOnNoYWhhYi1pbnF1aXJ5OmdldCIsIm9hazpibGFja2xpc3QtaW5xdWlyeTpnZXQiLCJvYWs6dHJhbnNhY3Rpb24taW5xdWlyeTpnZXQiLCJvYWs6cHJveHktYWNjb3VudC1pbnF1aXJ5OmV4ZWN1dGUiLCJjYXJkOmluZm9ybWF0aW9uOmdldCIsImNyZWRpdDpmYWNpbGl0eS1pbnF1aXJ5OmdldCIsImNyZWRpdDpiYWNrLWNoZXF1ZXM6Z2V0IiwiY3JlZGl0OmxvYW4taW5mbzpnZXQiLCJjcmVkaXQ6Z3VhcmFudGVlLWRldGFpbHM6Z2V0IiwiY3JlZGl0Omd1YXJhbnRlZS1jb2xsYXRlcmFsczpnZXQiLCJjcmVkaXQ6c2NvcmUtcmVxdWVzdDpwb3N0IiwiY3JlZGl0OnNjb3JlLXZlcmlmeTpwb3N0IiwiY3JlZGl0OnNjb3JlLXJlbmV3LXJlcXVlc3Q6cG9zdCIsImNyZWRpdDpzY29yZS1yZXBvcnQtaW5mbzpwb3N0IiwiY3JlZGl0OnNjb3JlLXJlbmV3LXJlcG9ydDpwb3N0IiwiY3JlZGl0OnNjb3JlLXJlcG9ydDpwb3N0IiwiY3JlZGl0OmNjLXNheWFkLWNoZXF1ZS1pbnF1aXJ5OmdldCIsImNyZWRpdDpjYy1zYXlhZC1pc3N1ZXItaW5xdWlyeS1jaGVxdWU6cG9zdCIsImNyZWRpdDpjYy1ndWFyYW50eS1pbnF1aXJ5OmdldCIsImJvb21yYW5nOndhZ2VzOmdldCIsImJvb21yYW5nOnRva2VuczpnZXQiLCJib29tcmFuZzp0b2tlbjpkZWxldGUiLCJib29tcmFuZzpzbXMtdmVyaWZ5OmV4ZWN1dGUiLCJib29tcmFuZzpzbXMtc2VuZDpleGVjdXRlIiwiZmFjaWxpdHk6Y2FyZC10by1pYmFuOmdldCIsImZhY2lsaXR5OmRlcG9zaXQtb3duZXItdmVyaWZpY2F0aW9uOmdldCIsImZhY2lsaXR5OmNjLWJhbmstaW5mbzpnZXQiLCJmYWNpbGl0eTpjYy1kZXBvc2l0LWliYW46Z2V0IiwiZmFjaWxpdHk6Y2FyZC10by1kZXBvc2l0OmdldCIsImJpbGxpbmc6Y2MtaW5xdWlyeTpnZXQiLCJiaWxsaW5nOmNjLWlucXVpcnktZGV0YWlsOmdldCIsImJpbGxpbmc6ZHJpdmluZy1vZmZlbnNlLWlucXVpcnk6Z2V0IiwiZmFjaWxpdHk6bG9hbi1pbmZvLXd0bHN0OmdldCIsImZhY2lsaXR5OmxvYW4taW5mby13dGxzdDpwb3N0IiwiYmlsbGluZzpjYy1uZWdhdGl2ZS1zY29yZTpnZXQiLCJmYWNpbGl0eTpjb21wYXJlLXR3by1pbWFnZXM6cG9zdCIsImZhY2lsaXR5OmZpbm5vdGV4dDpwb3N0IiwiZmFjaWxpdHk6Zmlubm90ZXh0LWlucXVpcnk6Z2V0IiwiZmFjaWxpdHk6ZGVwb3NpdC1pbmZvcm1hdGlvbjpnZXQiLCJ3YWxsZXQ6aW5mb3JtYXRpb246ZXhlY3V0ZSIsIndhbGxldDpjcmVhdGU6cG9zdCIsIndhbGxldDpjYXNoLW91dDpleGVjdXRlIiwid2FsbGV0OnRyYWNlTnVtYmVyOmdldCIsIndhbGxldDpjYXNoLWluOnBvc3QiLCJ3YWxsZXQ6d2FsbGV0LXAycDpwb3N0Iiwid2FsbGV0OnB1cmNoYXNlOnBvc3QiLCJ3YWxsZXQ6d2FsbGV0LWJhbGFuY2U6cG9zdCIsIndhbGxldDp3YWxsZXQtbGFzdC10cmFuc2FjdGlvbnM6cG9zdCIsIndhbGxldDptZXJjaGFudC1sYXN0LXRyYW5zYWN0aW9uczpwb3N0Iiwid2FsbGV0Om1lcmNoYW50LWJhbGFuY2U6cG9zdCIsIndhbGxldDp3YWxsZXQtZm9yZ290LXBpbjpwb3N0Iiwid2FsbGV0OmNyZWF0ZS1waW46cG9zdCIsIndhbGxldDpjaGFuZ2UtcGluOnBvc3QiLCJ3YWxsZXQ6dmVyaWZ5LXB1cmNoYXNlOnBvc3QiLCJ3YWxsZXQ6cmV2ZXJzZS1wdXJjaGFzZTpwb3N0Iiwid2FsbGV0OnRyYWNrLXB1cmNoYXNlOnBvc3QiLCJ3YWxsZXQ6bWVyY2hhbnQtY2hhbmdlLXBpbjpwb3N0IiwiZWNpdHk6bGV0dGVyOmdldCIsImVjaXR5OnBhcmtpbmdjYXBhY2l0eTpnZXQiLCJlY2l0eTpjYXJpbmZvOmdldCIsImVjaXR5OmNhcmR1dGllczpnZXQiLCJlY2l0eTpidXMtZXRhOmdldCIsImVjaXR5OmFsbGhpZ2h3YXl0b2xsOmdldCIsImVjaXR5OmNhcmJpbGwtYW5udWFsdG9sbDpnZXQiLCJlY2l0eTpjYXJiaWxsLXRlaHJhbnRvbGxzOmdldCIsImVjaXR5OnRvbGwtbGlzdC1ieS1jaXR5OmdldCIsImVjaXR5OnRvbGwtY2l0eWxpc3Q6Z2V0IiwiZWNpdHk6Y2FyYmlsbC1mcmVld2F5LXRvbGxzc3VtbWFyeTpnZXQiLCJlY2l0eTphaXItcG9sbHV0aW9uOmdldCIsImVjaXR5OmNjLXBvc3RhbC1jb2RlLWlucXVpcnk6Z2V0IiwicmVmdW5kOnRyYWNrLXJlZnVuZC13aXRoLWNhcmQ6Z2V0IiwicmVmdW5kOmdyb3VwLXJlYWx0aW1lLWRlcG9zaXQ6Z2V0IiwiZmFjaWxpdHk6c21zLXNoYWhrYXItc2VuZDpnZXQiLCJmYWNpbGl0eTpzbXMtc2hhaGthci12ZXJpZmljYXRpb246Z2V0IiwiZmFjaWxpdHk6Y29tcGFyZS1saXZlLWltYWdlLXdpdGgtbmF0aW9uYWwtY2FyZC1pbWFnZTpwb3N0IiwiZmFjaWxpdHk6Y29tcGFyZS1saXZlLXZpZGVvLXdpdGgtbmF0aW9uYWwtY2FyZC1pbWFnZTpwb3N0Iiwia3ljOmNvbXBhcmUtbGl2ZS1pbWFnZS13aXRoLW5hdGlvbmFsLWNhcmQtaW1hZ2U6cG9zdCIsImt5Yzpjb21wYXJlLXR3by1pbWFnZXM6cG9zdCIsImt5Yzpjb21wYXJlLWxpdmUtdmlkZW8td2l0aC1uYXRpb25hbC1jYXJkLWltYWdlOnBvc3QiLCJreWM6bW9iaWxlLWNhcmQtdmVyaWZpY2F0aW9uOnBvc3QiLCJreWM6b2NyLXZlcmlmaWNhdGlvbjpwb3N0Iiwia3ljOm5pZC1pbnF1aXJ5OmdldCIsImt5YzpkZWF0aC1zdGF0dXMtaW5xdWlyeTpnZXQiLCJreWM6bWlsaXRhcnktaW5xdWlyeTpnZXQiLCJreWM6Y2MtbmlkLXZlcmlmaWNhdGlvbjpnZXQiLCJmYWNpbGl0eTpkZWZpbmUtY3VzdG9tZXItYWNjb3VudC12ZXJpZnktc21zOmdldCIsImZhY2lsaXR5OmNjLXZlcmlmeS1kZWZpbmUtY3VzdG9tZXItYWNjb3VudC13aXRob3V0LXNtczpwb3N0IiwiZmFjaWxpdHk6ZGVmaW5lLWN1c3RvbWVyLWFjY291bnQtc3RlcDE6cG9zdCIsImZhY2lsaXR5OmRlZmluZS1jdXN0b21lci1hY2NvdW50LXVwbG9hZC1tZWRpYTpwb3N0IiwiZmFjaWxpdHk6ZGVmaW5lLWN1c3RvbWVyLWFjY291bnQ6cHV0IiwiZmFjaWxpdHk6ZGVmaW5lLWN1c3RvbWVyLWFjY291bnQtaW5xdWlyeTpnZXQiLCJmYWNpbGl0eTpjb25maXJtLWRlZmluZS1jdXN0b21lci1hY2NvdW50OnBvc3QiLCJreWM6Y2MtdmVyaWZ5LWRlZmluZS1jdXN0b21lci1hY2NvdW50LXdpdGhvdXQtc21zOnBvc3QiLCJreWM6ZGVmaW5lLWN1c3RvbWVyLWFjY291bnQtdmVyaWZ5LXNtczpnZXQiLCJreWM6ZGVmaW5lLWN1c3RvbWVyLWFjY291bnQtc3RlcDE6cG9zdCIsImt5YzpkZWZpbmUtY3VzdG9tZXItYWNjb3VudC1pbnF1aXJ5OmdldCIsImt5YzpkZWZpbmUtY3VzdG9tZXItYWNjb3VudC11cGxvYWQtbWVkaWE6cG9zdCIsImt5YzpkZWZpbmUtY3VzdG9tZXItYWNjb3VudDpwdXQiLCJreWM6Y29uZmlybS1kZWZpbmUtY3VzdG9tZXItYWNjb3VudDpwb3N0Iiwia3ljOnNtcy1zaGFoa2FyLXNlbmQ6Z2V0Iiwia3ljOmFwcGx5LWRlYml0LWNhcmQ6cG9zdCIsImt5YzphY2NvdW50LWNvbmRpdGlvbi12ZXJpZnktc21zOmdldCIsImt5YzphY2NvdW50LWNvbmRpdGlvbi11cGxvYWQtbWVkaWE6cG9zdCIsImt5YzphY2NvdW50LWNvbmRpdGlvbi1zbXMtc2hhaGthci1zZW5kOmdldCIsImt5Yzp1cGRhdGUtYWNjb3VudC1jb25kaXRpb246cG9zdCIsImt5YzppbnF1aXJ5LWFjY291bnQtY29uZGl0aW9uOmdldCIsImt5YzphY2NvdW50LWNvbmRpdGlvbi1yZXBvcnQ6Z2V0Iiwib2FrOnVwZGF0ZS1hY2NvdW50LWNvbmRpdGlvbi13aXRob3V0LWF1dGhvcml6YXRpb246cG9zdCJdLCJ0eXBlIjoiQ0xJRU5ULUNSRURFTlRJQUwiLCJjbGllbnRJbmZvIjoiNDlEc1FjUm5teUJxenJRVUJiSlY4dzV2VXRwNC9XUlJqTFpWeUZUQiIsImJhbmsiOiIwNjIiLCJpYXQiOjE2NzI1NjQwOTUsImV4cCI6MTY3MzQyODA5NX0.BsUgIGvvLnDox-k_E7CenlXIknVMVttRbjV8a-QghtoNEl4cLGyfQIRXwweBR9KYoplHJOMGIYawjm-9rD1pbhPzgEoCrd_2AuIkeTbay65KjL5K7mar87tRKQkCVJqTnMVxtQ1oUWoVOgiwl5Z9leE1RI8uV7Daek8aixi41fBlDJMVN5BDN8GFV93f1mxewXyFKmvojlrEHzAfx48I5Gicuf0Dh7c_Wm-Z17xj4AO_CbUxlwoI6Hhy7lL7yV2J3iKC_KQNOmndaodjSrwz-snGfvLzxlgK30hiX61j1aQk7yyqIcRH3k9WlB8qthOiYrfozqfmm_namoe44-is0w"
+    private redisClient: RedisClientType;
+    private bearerToken: string = '';
 
-    constructor(private readonly CLIENT_ID: string, private readonly CLIENT_PASSWORD: string) {
+    constructor(private readonly CLIENT_ID: string, private readonly CLIENT_PASSWORD: string,
+                private readonly tokenRedisUrl?: string) {
         // Read config.yaml file
         try {
             let fileContents = fs.readFileSync('./config.yaml', 'utf8');
@@ -41,6 +44,34 @@ export class ClientSDK {
         } catch (e) {
             throw new Error(`Failed to load config from ${this.yamlConfigFilePath} file`);
         }
+
+        // Connect to redis
+        if (tokenRedisUrl) {
+            this.redisClient = createClient({
+                url: tokenRedisUrl
+            });
+
+            this.redisClient.on('error', (err) => {
+                console.log('Redis client connecting to specified url error: ', err)
+            });
+        } else {
+            console.log('No tokenRedisUrl provided, connecting to our local redis server');
+            this.redisClient = createClient();
+            this.redisClient.on('error', (err) => {
+                console.log('Redis client connecting to localhost error: ', err)
+            });
+        }
+    }
+
+    private async getBearerToken(): Promise<string> {
+        // Get the bearer token from redis
+        const token = this.redisClient.get('bearerToken');
+        if (token !== null) {
+            console.log('Token not found in redis')
+            return '';
+        }
+
+        return token;
     }
 
     async callService(serviceName: string, payload: any) {
