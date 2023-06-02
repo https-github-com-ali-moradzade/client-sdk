a1186a6ad1d62f1d2d5289b3b3482a7f1ca08dc1
Ali Moradzade <alimorizz1379@gmail.com>
2023-06-02 21:55:48 +0330

    exporting our class in index.ts, so we can use it inside other projects

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,21 @@
-import * as dotenv from 'dotenv';
+// import * as dotenv from 'dotenv';
 
-import {ClientSDK} from "./ClientSDK";
-import {v4 as uuid} from 'uuid';
+// import {ClientSDK} from "./ClientSDK";
+// import {v4 as uuid} from 'uuid';
 
-dotenv.config();
+// dotenv.config();
 
-(async () => {
-    const clientSDK = new ClientSDK(process.env.CLIENT_ID || '',
-        process.env.CLIENT_PASSWORD || '', process.env.CLIENT_NID || '', process.env.REDIS_URL);
-    const result = await clientSDK.callService('billingInquiry', {
-        trackId: uuid(),
-        type: "Tel",
-        parameter: "02177689361",
-        secondParameter: "MCI"
-    })
+// (async () => {
+//     const clientSDK = new ClientSDK(process.env.CLIENT_ID || '',
+//         process.env.CLIENT_PASSWORD || '', process.env.CLIENT_NID || '', process.env.REDIS_URL);
+//     const result = await clientSDK.callService('billingInquiry', {
+//         trackId: uuid(),
+//         type: "Tel",
+//         parameter: "02177689361",
+//         secondParameter: "MCI"
+//     })
+//
+//     console.log(result);
+// })()
 
-    console.log(result);
-})()
+export {ClientSDK} from './ClientSDK';
