e4dc24a0d7637b830ddbc9011e92ef1311156da1
Ali Moradzade <alimorizz1379@gmail.com>
2023-06-03 15:33:11 +0330

    standardizing restClient

---
+++ src/util/restClient.spec.ts
@@ -0,0 +1,28 @@
+import {describe, expect, it} from "vitest";
+import {restClient} from "./restClient";
+import {CLIENT_SDK} from "../config";
+
+describe('restClient', () => {
+    it('should call service', async () => {
+        // Arrange
+        const service = {
+            name: 'drivingOffense',
+            url: `https://api.staging.finnotech.ir/billing/v2/clients/${CLIENT_SDK.config.clientId}/drivingOffense`,
+            method: 'get',
+            scope: 'billing:driving-offense-inquiry:get',
+            payload: {
+                mobile: '09128485085',
+                plateNumber: '500734744',
+                nationalID: '0440299705',
+            }
+        };
+
+        // Act
+        const result = await restClient(service);
+
+        // Assert
+        expect(result).toBeDefined();
+        expect(result.status).toBeTypeOf('number');
+        expect(result.data).toBeDefined();
+    });
+});
--- src/util/restClient.ts
+++ src/util/restClient.ts
@@ -1,8 +1,29 @@
 import {Service} from "./readYml";
-import {getTokenFromRedis} from "../redis/queries";
-import axios, {AxiosError, AxiosRequestConfig} from "axios";
+import {getTokenFromRedis, setTokenInRedis} from "../redis/queries";
+import axios, {AxiosRequestConfig, AxiosResponse} from "axios";
+import {getToken} from "./getToken";
+
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+
 
 export async function restClient(service: Service) {
+    axios.interceptors.response.use((response: AxiosResponse) => {
+        return response;
+    }, async (error: any) => {
+        if (error.response.status === 401 || error.response.status === 403) {
+            const token = await getToken("https://api.staging.finnotech.ir", service.scope);
+            if (token) {
+                await setTokenInRedis(service.scope, token);
+
+                // Retry request
+                error.config.headers.Authorization = `Bearer ${await getTokenFromRedis(service.scope)}`;
+                return axios.request(error.config);
+            }
+        }
+
+        return error;
+    });
+
     const method = service.method;
     let trackId: string | undefined = undefined;
 
@@ -11,21 +32,21 @@ export async function restClient(service: Service) {
         delete service.payload.trackId;
     }
 
-    let result;
     const token = await getTokenFromRedis(service.scope)
+
     const config = {
         url: service.url,
         headers: {Authorization: `Bearer ${token}`},
         params: method === 'get' ? service.payload : {trackId},
-        data: method === 'post' ? service.payload : undefined
+        data: method === 'post' ? service.payload : undefined,
+        validateStatus: function () {
+            return true;
+        }
     } as AxiosRequestConfig;
 
-    try {
-        const {data} = await axios.request(config);
-        result = data;
-    } catch (e) {
-        throw new Error(`Failed to call service ${service.name}: ${(e as AxiosError).message}`);
+    const result = await axios.request(config);
+    return {
+        status: result.status,
+        data: result.data
     }
-
-    return result;
 }
